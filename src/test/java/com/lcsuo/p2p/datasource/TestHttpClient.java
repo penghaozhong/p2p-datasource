355ad9a36242f196c7dacaf0f84645c6ea15b4e5
penghaozhong <penghaozhong@163.com>
2015-11-27 16:45:14 +0800

    新增测试抓取main

---
+++ src/test/java/com/lcsuo/p2p/datasource/TestHttpClient.java
@@ -0,0 +1,22 @@
+package com.lcsuo.p2p.datasource;
+
+import com.alibaba.fastjson.JSONObject;
+import com.lcsuo.p2p.collect.dao.MongoDBUtils;
+import com.lcsuo.p2p.collect.datasource.DataSource;
+import com.lcsuo.p2p.collect.datasource.WdzjDataSource;
+import com.lcsuo.p2p.collect.entity.P2p;
+import com.lcsuo.p2p.collect.entity.WdzjPage;
+
+public class TestHttpClient {
+
+	public static void main(String[] args) {
+		DataSource dataSource = new WdzjDataSource();
+		WdzjPage page = dataSource.search("http://www.wdzj.com/front_select-plat");
+		for (P2p p2p : page.getList()) {
+			System.out.println(p2p.getPlatName());
+		}
+	}
+	
+	
+
+}
